1c5ef79757ef6f4538a7860ffed293828d73bdce
bogdanovbs07 <bogdanovbs07@yandex.ru>
2026-04-26 23:33:08 +0300

    Create questions.js

---
+++ src/data/questions.js
@@ -0,0 +1,68 @@
+export const questions = [
+  {
+    id: 1,
+    question: "Что такое HTML?",
+    options: [
+      "Язык программирования",
+      "Язык разметки гипертекста",
+      "База данных",
+      "Операционная система"
+    ],
+    correctAnswer: 1
+  },
+  {
+    id: 2,
+    question: "Какой тег используется для создания ссылки?",
+    options: [
+      "<link>",
+      "<a>",
+      "<href>",
+      "<url>"
+    ],
+    correctAnswer: 1
+  },
+  {
+    id: 3,
+    question: "Что делает CSS?",
+    options: [
+      "Обрабатывает данные",
+      "Стилизует веб-страницы",
+      "Создает базы данных",
+      "Управляет сервером"
+    ],
+    correctAnswer: 1
+  },
+  {
+    id: 4,
+    question: "Какой метод для вывода в консоль в JavaScript?",
+    options: [
+      "print()",
+      "echo()",
+      "console.log()",
+      "display()"
+    ],
+    correctAnswer: 2
+  },
+  {
+    id: 5,
+    question: "Что такое React?",
+    options: [
+      "База данных",
+      "JavaScript библиотека для создания UI",
+      "Язык программирования",
+      "Текстовый редактор"
+    ],
+    correctAnswer: 1
+  },
+  {
+    id: 6,
+    question: "Какой хук используется для состояния в React?",
+    options: [
+      "useEffect",
+      "useState",
+      "useContext",
+      "useReducer"
+    ],
+    correctAnswer: 1
+  }
+];
